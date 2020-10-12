b0457c51ef3bcff295ad3e9e9d4a928aa11d7767
Nick Walters <nswalters.s@gmail.com>
2020-10-12 18:26:12 -0500

    Initial commit

---
+++ animals.py
@@ -0,0 +1,135 @@
+from datetime import date
+
+
+class Llama:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = False
+
+
+class Goat:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = False
+
+
+class Duck:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = False
+
+
+class Turtle:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = False
+
+
+class Horse:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = False
+
+
+class Cow:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = False
+
+
+class Chicken:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = False
+
+
+class CopperHead:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = False
+
+
+class RattleSnake:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = False
+
+
+class GardenSnake:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = False
+
+
+class Anaconda:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = False
+
+
+class CoralSnake:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = False
+
+
+class Goose:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = False
+
+
+class Swan:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = False
+
+
+class Bass:
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+    # def __str__(self):
+    #    return f"My name is {self.name} and I am a {self.species}."
+
+
+def main():
+    myBass = Bass()
+    myBass.name = "Wall Singer"
+    myBass.species = "Black Bass"
+
+    print(myBass)
+
+
+main()
