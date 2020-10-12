8a2b0f1dcb2819c8513060342dabc4d716b99f5b
Nick Walters <nswalters.s@gmail.com>
2020-10-12 18:46:37 -0500

    refactored constructors to accept parameters

--- animals.py
+++ animals.py
@@ -2,132 +2,132 @@ from datetime import date
 
 
 class Llama:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = False
 
 
 class Goat:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = False
 
 
 class Duck:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = False
 
 
 class Turtle:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = False
 
 
 class Horse:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = False
 
 
 class Cow:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = False
 
 
 class Chicken:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = False
 
 
 class CopperHead:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = False
 
 
 class RattleSnake:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = False
 
 
 class GardenSnake:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = False
 
 
 class Anaconda:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = False
 
 
 class CoralSnake:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = False
 
 
 class Goose:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = False
 
 
 class Swan:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = False
 
 
 class Bass:
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
-    # def __str__(self):
-    #    return f"My name is {self.name} and I am a {self.species}."
+    def __str__(self):
+        return f"My name is {self.name} and I am a {self.species}."
 
 
 def main():
-    myBass = Bass()
-    myBass.name = "Wall Singer"
-    myBass.species = "Black Bass"
+    myBass = Bass("Wall Singer", "Black Bass")
+    # myBass.name = "Wall Singer"
+    # myBass.species = "Black Bass"
 
     print(myBass)
 
